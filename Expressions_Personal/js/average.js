589ddea5fae2de11d14204cc2b2effa5ad4da852
Raul <blackbee@fullsail.edu>
2013-11-08 06:02:04 -0500

    New folders

---
+++ Expressions_Personal/js/average.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+/*
+
+Student: Raul Medina
+October 2, 2013
+Version: 1.0A
+Assignment: 
+WPF-1
+
+
+*/
+
+
+var pizzaPie = 10;
+
+var pieSlice = pizzaPie * 6;
+
+var partyMembers = 15;
+
+var x = pieSlice/partyMembers;
+
+
+
+console.log(x);
+
+
+
+
+
+
+

398f7e8700efcaf9a8a1846e277ca7dba0fbb529
Raul <blackbee@fullsail.edu>
2013-11-23 21:53:07 -0500

    New Folders

---
+++ Conditionals_Industry/js/industry.js
@@ -0,0 +1,50 @@
+// JavaScript Document
+
+/*
+
+Student: Raul Medina
+October 2, 2013
+Version: 1.0A
+Assignment: Conditionals
+WPF-1
+
+
+*/
+	
+
+	
+var quart = 1;
+
+var gallon = quart / 4;
+
+var x ;
+
+
+
+
+
+
+
+	x=prompt("How many quarts are in one gallon?");
+
+
+if (x == gallon * 4) {
+	
+		
+	alert("You guess right!");
+	
+	
+} else {
+		
+	
+	alert("You guessed  wrong!");
+
+	}
+
+
+
+
+
+
+console.log(x);
+
